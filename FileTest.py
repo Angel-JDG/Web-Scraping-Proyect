3e9887e7d83848661e6eff2fe568a34b34695a5c
Angel-JDG <angeldejesus1103u@gmail.com>
2026-03-24 22:39:25 -0500

    renaming variable from lista_limpia to cleaned_list

--- FileTest.py
+++ FileTest.py
@@ -1,8 +1,8 @@
 Frase = input("write a list separated by commas: ")
 List_data= Frase.split(",")
-lista_limpia = []
+cleaned_list = []
 for item in List_data:
     item_clean = item.strip(",.!?;:()[]{}\"'").replace(" ", "").capitalize() # delete spaces and punctuation, and capitalize the first letter
     if item_clean: # check if the cleaned item is not empty
-        lista_limpia.append(item_clean) # add the cleaned item to the new list
-print (lista_limpia)
+        cleaned_list.append(item_clean) # add the cleaned item to the new list
+print (cleaned_list)
